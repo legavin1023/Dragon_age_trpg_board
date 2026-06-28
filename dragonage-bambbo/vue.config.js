05dd17b9143fb94ed4c66bf3ef87084edec6c5ff
huiseon <legavin1023@gmail.com>
2026-06-28 18:50:08 +0900

    깃페이지

--- dragonage-bambbo/vue.config.js
+++ dragonage-bambbo/vue.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require("@vue/cli-service");
 const path = require("path");
 
 module.exports = defineConfig({
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/Dragon_age_trpg_board/" // <-- 여기를 본인의 깃허브 레포지토리 이름으로 바꾸세요
+      : "/",
   transpileDependencies: true,
   configureWebpack: {
     resolve: {
